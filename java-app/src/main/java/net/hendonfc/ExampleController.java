882330bfa730e49c0e12d623354d9e1bbd1fe89c
phil rogers <philrogersuk@gmail.com>
2021-06-12 22:18:06 +0100

    Force build

--- java-app/src/main/java/net/hendonfc/ExampleController.java
+++ java-app/src/main/java/net/hendonfc/ExampleController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+
 @Controller
 public class ExampleController {
 
@@ -13,6 +15,7 @@ public class ExampleController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("timestamp", LocalDateTime.now());
         return "greeting";
     }
 
